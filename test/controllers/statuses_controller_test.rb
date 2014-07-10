4e76143f4f1a9b7c2b6d8e7be69f1aabccad6106
Jin Han Huang <huang_jin_han@hotmail.com>
2014-07-10 21:47:17 +0800

    Added tests for custom routes

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-
+   
 class StatusesControllerTest < ActionController::TestCase
   setup do
     @status = statuses(:one)
@@ -13,12 +13,19 @@ class StatusesControllerTest < ActionController::TestCase
 
   test "should get new" do
     get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render the new page when logged in" do
+    sign_in users(:jin)
+    get :new
     assert_response :success
   end
 
   test "should create status" do
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
+      post :create, status: { content: @status.content }
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -35,7 +42,7 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should update status" do
-    patch :update, id: @status, status: { content: @status.content, name: @status.name }
+    patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
@@ -46,4 +53,5 @@ class StatusesControllerTest < ActionController::TestCase
 
     assert_redirected_to statuses_path
   end
+
 end
